9daf0cf99ac10dec38ddb617759dba6e20d4517d
Artem Korsakov <kor@infra.guru>
2020-07-31 13:10:28 +0300

    Section 10.3 done

--- tests/src/test/scala/com/github/artemkorsakov/tests/cats/SectionTenPointFourTestSuite.scala
+++ tests/src/test/scala/com/github/artemkorsakov/tests/cats/SectionTenPointFourTestSuite.scala
@@ -1,9 +1,21 @@
 package com.github.artemkorsakov.tests.cats
 
+import cats.data.NonEmptyList
+import cats.data.Validated.{ Invalid, Valid }
+import com.github.artemkorsakov.cats.SectionTenPointFour._
 import org.scalatest.Matchers
 import org.scalatest.funsuite.AnyFunSuiteLike
 
 class SectionTenPointFourTestSuite extends AnyFunSuiteLike with Matchers {
-  test("10.4 Transforming Data") {}
+  test("10.4 Transforming Data") {
+    createUser("Noel", "noel@underscore.io") shouldBe Valid(User("Noel", "noel@underscore.io"))
+
+    createUser("", "dave@underscore.io@io") shouldBe Invalid(
+      NonEmptyList(
+        "Must be longer than 3 characters",
+        List("Must contain a single @ character")
+      )
+    )
+  }
 
 }
