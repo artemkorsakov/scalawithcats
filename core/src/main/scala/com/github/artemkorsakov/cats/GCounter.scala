6ffc6b1517a79356727e79dd8f3e7a4dadf2a3b7
Artem Korsakov <kor@infra.guru>
2020-08-04 13:58:12 +0300

    Section 11.3 done

--- core/src/main/scala/com/github/artemkorsakov/cats/GCounter.scala
+++ core/src/main/scala/com/github/artemkorsakov/cats/GCounter.scala
@@ -1,17 +1,20 @@
 package com.github.artemkorsakov.cats
 
-final case class GCounter(counters: Map[String, Int]) {
-  def increment(machine: String, amount: Int): GCounter = {
-    val value = amount + counters.getOrElse(machine, 0)
+import cats.instances.list._
+import cats.instances.map._
+import cats.kernel.CommutativeMonoid
+import cats.syntax.foldable._
+import cats.syntax.semigroup._
+
+final case class GCounter[A](counters: Map[String, A]) {
+  def increment(machine: String, amount: A)(implicit m: CommutativeMonoid[A]): GCounter[A] = {
+    val value = amount |+| counters.getOrElse(machine, m.empty)
     GCounter(counters + (machine -> value))
   }
 
-  def merge(that: GCounter): GCounter =
-    GCounter(that.counters ++ this.counters.map {
-      case (k, v) =>
-        k -> (v max that.counters.getOrElse(k, 0))
-    })
+  def merge(that: GCounter[A])(implicit b: BoundedSemiLattice[A]): GCounter[A] =
+    GCounter(this.counters |+| that.counters)
 
-  def total: Int =
-    counters.values.sum
+  def total(implicit m: CommutativeMonoid[A]): A =
+    this.counters.values.toList.combineAll
 }
--- tests/src/test/scala/com/github/artemkorsakov/tests/cats/CRDTsTestSuite.scala
+++ tests/src/test/scala/com/github/artemkorsakov/tests/cats/CRDTsTestSuite.scala
@@ -1,26 +1,39 @@
 package com.github.artemkorsakov.tests.cats
 
+import com.github.artemkorsakov.cats.BoundedSemiLattice._
 import com.github.artemkorsakov.cats.GCounter
 import org.scalatest.Matchers
 import org.scalatest.funsuite.AnyFunSuiteLike
 
 class CRDTsTestSuite extends AnyFunSuiteLike with Matchers {
   test("11.2.3 Exercise: GCounter Implementation") {
-    val a = GCounter(Map("A" -> 2, "B" -> 3))
-    a shouldBe GCounter(Map("A"                   -> 2, "B" -> 3))
-    a.increment("A", 4) shouldBe GCounter(Map("A" -> 6, "B" -> 3))
-    a.increment("B", 4) shouldBe GCounter(Map("A" -> 2, "B" -> 7))
+    val a = GCounter[Int](Map("A" -> 2, "B" -> 3))
+    a shouldBe GCounter[Int](Map("A"                   -> 2, "B" -> 3))
+    a.increment("A", 4) shouldBe GCounter[Int](Map("A" -> 4, "B" -> 3))
+    a.increment("B", 4) shouldBe GCounter[Int](Map("A" -> 2, "B" -> 4))
 
-    val b = GCounter(Map("A" -> 1, "B" -> 14))
-    b shouldBe GCounter(Map("A" -> 1, "B" -> 14))
+    val b = GCounter[Int](Map("A" -> 1, "B" -> 14))
+    b shouldBe GCounter[Int](Map("A" -> 1, "B" -> 14))
 
     val c = a.increment("A", 4).increment("B", 4)
-    c shouldBe GCounter(Map("A" -> 6, "B" -> 7))
-    c.total shouldBe 13
+    c shouldBe GCounter(Map("A" -> 4, "B" -> 4))
+    c.total shouldBe 4
 
     val d = c.merge(b)
-    d shouldBe GCounter(Map("A" -> 6, "B" -> 14))
-    d.total shouldBe 20
+    d shouldBe GCounter(Map("A" -> 4, "B" -> 14))
+    d.total shouldBe 14
+  }
+
+  test("11.3.2 Exercise: BoundedSemiLattice Instances") {
+    intInstance.combine(1, 2) shouldBe 2
+    intInstance.combine(2, 1) shouldBe 2
+    intInstance.empty shouldBe 0
+    intInstance.combine(1, intInstance.empty) shouldBe 1
+
+    setInstance.combine(Set(1), Set(2)) shouldBe Set(1, 2)
+    setInstance.combine(Set(2), Set(1)) shouldBe Set(1, 2)
+    setInstance[Int].empty shouldBe Set.empty[Int]
+    setInstance.combine(Set(1, 2, 3), setInstance[Int].empty) shouldBe Set(1, 2, 3)
   }
 
 }
